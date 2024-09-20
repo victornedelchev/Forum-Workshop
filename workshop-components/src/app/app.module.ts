3157378b6e9dc8ab470f1c4092d4d7d87c20d621
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-09-20 15:40:05 +0300

    Add theme-list and post-list components

--- workshop-components/src/app/app.module.ts
+++ workshop-components/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { MainComponent } from './main/main.component';
+import { ThemesListComponent } from './themes-list/themes-list.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent],
+  declarations: [AppComponent, MainComponent, ThemesListComponent, PostsListComponent],
   imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
