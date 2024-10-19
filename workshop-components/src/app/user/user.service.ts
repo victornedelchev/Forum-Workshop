42a3d2ada1344a8d350d3c1aa455710ac98971c4
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-10-19 14:17:09 +0300

    Add fake login functionality

---
+++ workshop-components/src/app/user/user.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { User } from '../types/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  user: User | undefined;
+  USER_KEY = '[user]';
+
+  get isLogged(): boolean {
+    return !!this.user;
+  }
+
+  constructor() {
+    try {
+      const isUser = localStorage.getItem(this.USER_KEY) || '';
+      this.user = JSON.parse(isUser);
+    } catch (err) {
+      this.user = undefined;
+    }
+  }
+
+  login(): void {
+    this.user = {
+      username: 'victoryo',
+      email: 'victor@abv.bg',
+      telephone: '0898123456',
+      password: '123123',
+    };
+
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+  }
+
+  logout(): void {
+    this.user = undefined;
+    localStorage.removeItem(this.USER_KEY);
+  }
+}
