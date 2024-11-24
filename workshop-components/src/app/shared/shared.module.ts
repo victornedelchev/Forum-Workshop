6d5a3561a37e5b6c9f2449cb60e0735a66dc0d2a
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-11-24 17:02:12 +0200

    Add slice pipe on theme name

--- workshop-components/src/app/shared/shared.module.ts
+++ workshop-components/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { AppEmailDirective } from './validators/app-email.directive';
+import { SlicePipe } from './pipes/slice.pipe';
 
 @NgModule({
-  declarations: [LoaderComponent, AppEmailDirective],
+  declarations: [LoaderComponent, AppEmailDirective, SlicePipe],
   imports: [CommonModule],
-  exports: [LoaderComponent, AppEmailDirective],
+  exports: [LoaderComponent, AppEmailDirective, SlicePipe],
 })
 export class SharedModule {}
